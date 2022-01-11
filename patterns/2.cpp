fcfba4e223d13a554aa5d3f7c4b66905b2bbfd8a
Vishakha Kathait <Vishakhakathait020@gmail.com>
2022-01-12 00:58:31 +0530

    Create 2.cpp

---
+++ patterns/2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+   for(int i=1; i<6; i++)
+         {
+             for(int j=6-i; j>0;j--)
+             {
+                 cout<<"*";
+             }
+             cout<<"\n";
+         }   
+
+
+    return 0;
+}
+  
+
+
+
+
+
+
+*/
+*****
+****
+***
+**
+*
+/*
